049b109d54bf55e0b1894b75e1aec67ab9dc3703
Clara Cidrás Fernández <91012917+claracidras@users.noreply.github.com>
2024-04-28 12:09:48 +0200

    Added Options menu features and music (options don't work properly)

---
+++ TrashGame/Assets/OptionsMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void FullScreen(bool fullScreen){
+        Screen.fullScreen = fullScreen;
+    }
+
+    public void ChangeVolume(float volume){
+        audioMixer.SetFloat("Volume", volume);
+    } 
+
+    public void ChangeQuality(int index){
+        QualitySettings.SetQualityLevel(index);
+    }    
+
+}
