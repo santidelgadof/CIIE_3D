f62b3678ecbe604c37a355dae5ce289779881a4f
Cristian Gonzalez Trillo <cristian.gonzalez@nomasystems.com>
2024-05-06 18:12:22 +0200

    feat: options
    Co-Authored-By: Clara Cidrás Fernández <91012917+claracidras@users.noreply.github.com>

---
+++ TrashGame/Assets/OptionsMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void ChangeVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void ChangeQuality(int index){
+        QualitySettings.SetQualityLevel(index);
+    }
+}
